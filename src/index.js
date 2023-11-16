5817551093c48125aba41a3dd629fc01effa7504
Rajeev Ranjan <rajeev.ranjan@danfoss.com>
2023-11-16 16:47:37 +0530

    commented some code for resolving cors

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(cors({origin: 'https://main--mellifluous-choux-0b1e2a.netlify.app/',optionsSuccessStatus: 200 }))
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin','https://main--mellifluous-choux-0b1e2a.netlify.app/');
+    // res.setHeader('Access-Control-Allow-Origin','https://main--mellifluous-choux-0b1e2a.netlify.app/');
     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
     next();
     });
