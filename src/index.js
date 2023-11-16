49ba03466435ca56e1f3671dc141c156658b200e
Rajeev Ranjan <rajeev.ranjan@danfoss.com>
2023-11-16 16:42:58 +0530

    added installed cors and added code to solve cors issue

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express') 
 const connectToDb  = require('./database')
 const companiesRouter = require('./routes/companies')
+const cors  = require('cors')
 const app = express()
 
 
+app.use(cors({origin: 'https://main--mellifluous-choux-0b1e2a.netlify.app/',optionsSuccessStatus: 200 }))
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin','https://main--mellifluous-choux-0b1e2a.netlify.app/');
     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
