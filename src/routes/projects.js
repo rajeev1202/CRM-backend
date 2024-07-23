09d02d73444df8e7aaf712666828be3661eb5d3e
PriteshGandhi1993 <pritesh.gandhi1993@gmail.com>
2024-07-22 21:24:22 -0400

    NOCNVCRM-30 added project addition API

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -20,13 +20,28 @@ router.get("/projects/get", async (req, res) => {
     try{
         const data = await Projects.aggregate([ { $lookup: { from:"companies", localField:"customerId", foreignField:"_id",pipeline: [
             { $project: {"name":1}}], as:"companyDetails"}},
-            { $unwind : { path : "$quotationId" , "includeArrayIndex" : "rank"}},
+            { $unwind : { path : "$quotationId" , "includeArrayIndex" : "rank", "preserveNullAndEmptyArrays": true}},
             { $lookup: { from:"quotations", localField:"quotationId", foreignField:"_id", as:"quotationData"}},
         ])
         res.status(200).json(data)
     } catch(err){
         res.status(500).json({ message: err.message });
     }
+});
+
+router.post("/projects/save", async (req, res) => {
+  try{
+    console.log("save proj data: ", req.body);
+    let data = {
+      customerId: req.body.customerId,
+      name: req.body.projectName,
+    }
+    console.debug("project req data",req.body);
+    const newProject = await Projects.create(data)
+    res.status(200).json(newProject)
+  } catch(err){
+      res.status(500).json({ message: err.message });
+  }
 });
 
 
